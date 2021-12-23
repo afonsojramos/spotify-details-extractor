eebee8f956ae4e581f1e93e0262d1ed206a7956d
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-12-23 21:39:28 +0000

    change query to location-based

--- firefox/extractor.js
+++ firefox/extractor.js
@@ -1,11 +1,8 @@
 extractorReceiver = () => {
-  const ARTIST_CLASS = 'a.TQQSam9tGsRXGj4aGFSc';
-  const IMG_CLASS = 'div.AGotAT_LDWXQc2YQCZES img.lZHD9qKHJOlxxGijUBTE';
-
   const album = {
     title: document.querySelector('h1').textContent,
-    artist: document.querySelector(ARTIST_CLASS).textContent,
-    image: document.querySelector(IMG_CLASS).src,
+    artist: document.querySelector('div > h2 + span + div > div > a').textContent,
+    image: document.querySelector('section > div > div > div > img').src,
     url: window.location.href.match(/https:\/\/open\.spotify\.com\/\w*\/\w*/)[0],
   };
   navigator.clipboard.writeText(JSON.stringify(album));
